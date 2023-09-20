1641b5aaf6019cc0433bd1994c418425ccfe6d32
Nguyen Phuoc Duy Khang <duykhangart@gmail.com>
2023-09-20 20:17:08 +0700

    Setup Router, Functions Products Page

--- coffee-shop-backend/server.js
+++ coffee-shop-backend/server.js
@@ -13,11 +13,13 @@ const io = new Server(server, {
 
 const User = require("./models/User");
 const userRoutes = require("./routes/userRoute");
+const productRoutes = require("./routes/productRoute");
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use("/users", userRoutes);
+app.use("/products", productRoutes);
 
 server.listen(8080, () => {
   console.log("server is running at port", 8080);
